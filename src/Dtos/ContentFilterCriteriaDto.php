6efe5233d66f5d1635f30f021164100e27f52890
Mehmet Güneş <mehmtgunes@teknsyon.com>
2022-12-30 23:10:41 +0300

    changed name

--- src/Dtos/ContentFilterCriteriaDto.php
+++ src/Dtos/ContentFilterCriteriaDto.php
@@ -16,20 +16,20 @@ use Illuminate\Support\Collection;
         $criteria = new Collection();
 //        $user = auth()->user();
 
-        if ($request->has('title')) {
-            $criteria->push(new LikeCriterion('hh5p_contents.title', $request->input('title')));
-        }
-//        if ($user->can(H5PPermissionsEnum::H5P_LIST) && $request->has('author_id')) {
-        if ($request->has('author_id')) {
-            $criteria->push(new EqualCriterion('hh5p_contents.user_id', $request->input('author_id')));
-        }
-//        if (!$user->can(H5PPermissionsEnum::H5P_LIST) && $user->can(H5PPermissionsEnum::H5P_AUTHOR_LIST)) {
-        if (auth()->user()) {
-            $criteria->push(new EqualCriterion('hh5p_contents.user_id', auth()->user()->getKey()));
-        }
-        if ($request->has('library_id')) {
-            $criteria->push(new EqualCriterion('hh5p_contents.library_id', $request->input('library_id')));
-        }
+//        if ($request->has('title')) {
+//            $criteria->push(new LikeCriterion('hh5p_contents.title', $request->input('title')));
+//        }
+////        if ($user->can(H5PPermissionsEnum::H5P_LIST) && $request->has('author_id')) {
+//        if ($request->has('author_id')) {
+//            $criteria->push(new EqualCriterion('hh5p_contents.user_id', $request->input('author_id')));
+//        }
+////        if (!$user->can(H5PPermissionsEnum::H5P_LIST) && $user->can(H5PPermissionsEnum::H5P_AUTHOR_LIST)) {
+//        if (auth()->user()) {
+//            $criteria->push(new EqualCriterion('hh5p_contents.user_id', auth()->user()->getKey()));
+//        }
+//        if ($request->has('library_id')) {
+//            $criteria->push(new EqualCriterion('hh5p_contents.library_id', $request->input('library_id')));
+//        }
 
         return new self($criteria);
     }
